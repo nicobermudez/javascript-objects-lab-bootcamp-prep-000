66b22aabbf513872172e38d2d170b21f9d418655
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-08 17:42:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = new Object({});
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var object2 = Object.assign({}, object, {[key]:value});
+  return object2;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var object2 = delete
+}
